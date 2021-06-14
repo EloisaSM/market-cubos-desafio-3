a8bf737d35dcade17d591be52b2482b5f257c0be
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-14 20:28:42 -0300

    atualizar perfil

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,4 +1,4 @@
-const { getPerfilUser } = require("../services/services");
+const { getPerfilUser, updateProfile } = require("../services/services");
 
 const perfil = async (req, res) => {
   const {
@@ -19,4 +19,22 @@ const perfil = async (req, res) => {
   }
 };
 
-module.exports = { perfil };
+const perfilUpdate = async (req, res) => {
+  const {
+    body,
+    usuario: { id },
+  } = req;
+
+  if (!Object.keys(body).length) {
+    return res.status(400).json("Informe os campos a serem alterados");
+  }
+
+  try {
+    const profileUpdated = await updateProfile(body, id);
+    return res.json(profileUpdated);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { perfil, perfilUpdate };
