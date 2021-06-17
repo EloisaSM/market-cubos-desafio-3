8e4a4921612ec758691e26ab1b9c5ae172f3436a
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-17 10:17:09 -0300

    estrutura componente Produtos

--- front-end/src/paginas/Produtos/index.js
+++ front-end/src/paginas/Produtos/index.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+import ProdutoCard from "../../components/ProdutoCard/ProdutoCard";
+import "./style.css";
 
 function ListaProdutos() {
+  const history = useHistory();
+
   return (
-    <div>
-      Lista de Produtos
-      <Link to="/produtos/novo">Editar Perfil</Link>
+    <div className="conteudo-container">
+      <Navbar />
+      <div className="loja-container">
+        <Typography variant="h3">Nome da Loja</Typography>
+        <Typography variant="subtitle1">Seus Produtos</Typography>
+
+        <div className="container-produtos">
+          <ProdutoCard />
+        </div>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => history.push("/produtos/novo")}
+        >
+          <Typography variant="button" display="block" gutterBottom>
+            adicionar produto
+          </Typography>
+        </Button>
+      </div>
     </div>
   );
 }
