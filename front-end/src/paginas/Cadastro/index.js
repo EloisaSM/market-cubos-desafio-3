0f6ae7e3afe27f55c0a3047b46d0b4095c68a1ca
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-17 10:13:10 -0300

    adicionando estado erro e load, requisição com back-end

--- front-end/src/paginas/Cadastro/index.js
+++ front-end/src/paginas/Cadastro/index.js
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Alert from "@material-ui/lab/Alert";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import useStyles from "./style";
 
 import { Link, useHistory } from "react-router-dom";
@@ -17,19 +19,42 @@ function Cadastro() {
     watch,
   } = useForm();
   const history = useHistory();
+  const [erro, setErro] = useState("");
+  const [open, setOpen] = useState(false);
 
-  function onSubmit(data) {
-    console.log(data);
+  async function onSubmit(data) {
+    setErro("");
+    setOpen(true);
 
-    if (data.repetirSenha !== data.senha) {
-      return "Senhas devem ser repetidas";
+    try {
+      const resposta = await fetch("http://localhost:8000/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const dados = await resposta.json();
+
+      console.log(resposta);
+
+      setOpen(false);
+
+      // if (!resposta.ok) {
+      //   setErro(dados);
+      //   return;
+      // }
+
+      history.push("/");
+    } catch (error) {
+      setErro(error.message);
     }
-
-    history.push("/");
   }
   return (
     <div className="login-container">
       <Typography variant="h4">Criar uma conta </Typography>
+
       <form
         className={classes.root}
         noValidate
@@ -80,6 +105,10 @@ function Cadastro() {
         <Button variant="contained" color="primary" type="submit">
           CRIAR CONTA
         </Button>
+
+        {erro && <Alert severity="error">{erro}</Alert>}
+        {open && <CircularProgress />}
+
         <span>
           Primeira vez aqui?
           <Link to="/" style={{ color: "blue" }}>
