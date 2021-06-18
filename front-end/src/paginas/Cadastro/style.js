20f5b4d5d7d016dd379d03f23d5693f002502dcc
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-18 11:01:43 -0300

    estilo login e cadastro

--- front-end/src/paginas/Cadastro/style.js
+++ front-end/src/paginas/Cadastro/style.js
@@ -4,7 +4,29 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "grid",
     placeContent: "center",
-    gap: "15px",
+    placeItems: "center",
+    gap: 40,
+
+    padding: "80px 0",
+    width: 390,
+
+    backgroundColor: "#ffff",
+
+    boxShadow:
+      "0px 8px 9px -5px rgba(0, 0, 0, 0.2), 0px 15px 22px 2px rgba(0, 0, 0, 0.14), 0px 6px 28px 5px rgba(0, 0, 0, 0.12)",
+
+    borderRadius: 16,
+  },
+  margin: {
+    marginRight: 5,
+  },
+  containerContent: {
+    display: "grid",
+    placeContent: "center",
+    placeItems: "center",
+
+    height: "100vh",
+    padding: 45,
   },
   Typography: {
     h4: {
--- front-end/src/paginas/Login/style.js
+++ front-end/src/paginas/Login/style.js
@@ -5,7 +5,27 @@ const useStyles = makeStyles((theme) => ({
     display: "grid",
     placeContent: "center",
     placeItems: "center",
-    gap: "15px",
+    gap: 40,
+
+    width: 390,
+    height: 590,
+
+    backgroundColor: "#ffff",
+
+    boxShadow:
+      "0px 8px 9px -5px rgba(0, 0, 0, 0.2), 0px 15px 22px 2px rgba(0, 0, 0, 0.14), 0px 6px 28px 5px rgba(0, 0, 0, 0.12)",
+
+    borderRadius: 16,
+  },
+  margin: {
+    marginRight: 5,
+  },
+  containerContent: {
+    display: "grid",
+    placeContent: "center",
+    placeItems: "center",
+
+    height: "100vh",
   },
 }));
 
