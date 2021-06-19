881f945e752eac47daf665730e91a9cf56bc891e
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-18 22:08:56 -0300

    alteração botao e estilo dos componentes novo produto

--- front-end/src/paginas/NovoProduto/style.js
+++ front-end/src/paginas/NovoProduto/style.js
@@ -1,17 +1,46 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 export const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    flexDirection: "column",
     gap: "48px",
   },
   subtitle: {
-    margin: "86px 0px 57px 0px ",
     fontSize: "34px",
   },
   precoEstoqueContainer: {
     display: "flex",
     gap: "24px",
   },
+  lojaContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 30,
+
+    padding: "78px 0 24px 74px",
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 24,
+  },
+  divider: {
+    marginTop: 50,
+    width: "150%",
+  },
+
+  link: {
+    marginRight: 24,
+    textDecoration: "underline",
+  },
 }));
+
+export const ColorButton = withStyles((theme) => ({
+  root: {
+    backgroundColor: "#007DFF",
+    "&:hover": {
+      backgroundColor: "rgba(0, 0, 0, 0.04)",
+    },
+  },
+}))(Button);
