ec0fa298e160b00681dd0ce40267d62e42cd862d
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-18 22:07:59 -0300

    imports fucionalidade botao

--- front-end/src/paginas/Perfil/index.js
+++ front-end/src/paginas/Perfil/index.js
@@ -1,19 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import useStyles from "./style";
+import Divider from "@material-ui/core/Divider";
+import { ColorButton } from "./style";
 
 import useAuth from "../../hook/useAuth";
 
-import NavBar from "../../components/Navbar";
+import NavBar from "../../components/Navbar/Navbar";
 
 function Perfil() {
   const classes = useStyles();
-  const [erro, setErro] = useState("");
   const [usuario, setUsuario] = useState({});
+  const history = useHistory();
 
   const { token } = useAuth();
 
@@ -38,38 +40,53 @@ function Perfil() {
     <div className={classes.root}>
       <NavBar />
 
-      <div className={classes.perfilContainer}>
+      <div className={classes.perfilInfo}>
         <Typography variant="h3">Nome da Loja</Typography>
         <Typography variant="subtitle1">Perfil</Typography>
 
-        <form className={classes.root} noValidate autoComplete="off">
+        <form className={classes.formContainer} noValidate autoComplete="off">
           <TextField
+            className={classes.input}
             disabled
-            id="standard-disabled"
+            id="nome"
             label="Nome"
             value={usuario.nome || ""}
           />
 
           <TextField
+            className={classes.input}
             disabled
-            id="standard-disabled"
+            id="nome-Loja"
             label="Nome da Loja"
             value={usuario.nome_loja || ""}
           />
 
           <TextField
+            className={classes.input}
             disabled
-            id="standard-disabled"
+            id="email"
             label="E-mail"
             value={usuario.email || ""}
           />
         </form>
 
-        <Button variant="contained" color="primary">
-          <Typography variant="button" display="block" gutterBottom>
-            editar perfil
-          </Typography>
-        </Button>
+        <Divider />
+
+        <div>
+          <Link className={classes.link} to="/perfil">
+            CANCELAR
+          </Link>
+
+          <ColorButton
+            variant="contained"
+            color="primary"
+            onClick={() => history.push("/perfil/editar")}
+          >
+            <Typography variant="button" display="block" gutterBottom>
+              editar perfil
+            </Typography>
+          </ColorButton>
+        </div>
       </div>
     </div>
   );
