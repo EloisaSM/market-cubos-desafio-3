2fff18d87131bfb2a1c32c0cd66da590f614df0c
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-19 15:40:54 -0300

    alteração botao,inclusao backdrop e container dos produtos

--- front-end/src/paginas/Produtos/style.js
+++ front-end/src/paginas/Produtos/style.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
   },
   lojaContainer: {
     padding: "78px 0px 24px 74px",
-    // minWidth: "80%",
+    minWidth: "80%",
 
     display: "flex",
     flexDirection: "column",
@@ -23,11 +23,15 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     gap: 24,
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
 }));
 
 export const ColorButton = withStyles((theme) => ({
   root: {
-    width: "18%",
+    maxWidth: "18%",
     backgroundColor: "#007DFF",
     "&:hover": {
       backgroundColor: "rgba(0, 0, 0, 0.04)",
