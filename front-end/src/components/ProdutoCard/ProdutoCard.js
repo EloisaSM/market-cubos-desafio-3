e6ad631206d966909b49bf681d3975e7fbbed17b
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-06-20 10:00:13 -0300

    alterando css produto card

--- front-end/src/components/ProdutoCard/ProdutoCard.js
+++ front-end/src/components/ProdutoCard/ProdutoCard.js
@@ -2,25 +2,13 @@ import React from "react";
 
 import "./style.css";
 
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  media: {
-    height: 240,
-  },
-  produtoInfo: {
-    marginTop: "11px",
-  },
-  produtoEstoque: {
-    marginRight: "64px",
-    color: "rgba(101, 101, 101, 0.87)",
-  },
-});
+import { useStyles } from "./style";
 
 function ProdutoCard({
   nome,
---
+++ front-end/src/components/ProdutoCard/style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+  media: {
+    height: 240,
+  },
+  produtoInfo: {
+    marginTop: "11px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  produtoEstoque: {
+    marginRight: "64px",
+    color: "rgba(101, 101, 101, 0.87)",
+  },
+});
